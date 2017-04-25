3e59e4c189a5005be22abf929a969ec97e3459c4
Sean McHenry <sean@seanmchenry.com>
2017-04-25 18:09:18 -0400

    Updated composer.lock and environment config files

--- config/environments/development.php
+++ config/environments/development.php
@@ -1,5 +1,11 @@
 <?php
-/** Development */
-define('SAVEQUERIES', true);
+/**
+ * development.php
+ */
+
 define('WP_DEBUG', true);
+// define('WP_DEBUG_DISPLAY', false);
+// define('WP_DEBUG_LOG', true);
+
+define('SAVEQUERIES', true);
 define('SCRIPT_DEBUG', true);
--- config/environments/production.php
+++ config/environments/production.php
@@ -1,7 +1,13 @@
 <?php
-/** Production */
+/**
+ * production.php
+ */
+
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
-/** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+define('WP_POST_REVISIONS', 5);
+
+// define('AUTOSAVE_INTERVAL', 180);
+// define('WP_MEMORY_LIMIT', '128M');
